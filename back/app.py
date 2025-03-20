7be592cc3016d8c54c780e21ea875efb7e5a3a67
Fedesin <fedesimone31@gmail.com>
2025-03-20 15:57:58 -0300

    borro mail-password del env y el app.py

--- back/app.py
+++ back/app.py
@@ -17,7 +17,6 @@ app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
 app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT", 587))
 app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS", "true").lower() in ["true", "1", "yes"]
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
-app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
 
 # Inicializamos las extensiones con la app
