17040b3457ab9b7bd9a7d884bd0e9adfdb6e01fa
Fedesin <fedesimone31@gmail.com>
2025-03-25 11:49:23 -0300

    modifico un poco la pantalla de login en el front

--- front/src/components/LoginScreen.tsx
+++ front/src/components/LoginScreen.tsx
@@ -5,49 +5,6 @@ import React, { useEffect, useRef, useState } from "react";
 import { useBoundStore } from "~/hooks/useBoundStore";
 import { useRouter } from "next/router";
 
-export const FacebookLogoSvg = (props: ComponentProps<"svg">) => {
-  return (
-    <svg width="12" height="22" viewBox="0 0 12 22" {...props}>
-      <title>Fill 4</title>
-      <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-        <g fill="#3C5A99">
-          <g>
-            <g>
-              <path d="M7.275 21.584v-9.845h3.305l.495-3.837h-3.8v-2.45c0-1.111.309-1.868 1.902-1.868l2.032-.001V.15C10.857.104 9.65 0 8.249 0c-2.93 0-4.936 1.788-4.936 5.072v2.83H0v3.837h3.313v9.845h3.962z" />
-            </g>
-          </g>
-        </g>
-      </g>
-    </svg>
-  );
-};
-
-export const GoogleLogoSvg = (props: ComponentProps<"svg">) => {
-  return (
-    <svg viewBox="0 0 48 48" {...props}>
-      <g>
-        <path
-          fill="#EA4335"
-          d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"
-        ></path>
-        <path
-          fill="#4285F4"
-          d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"
-        ></path>
-        <path
-          fill="#FBBC05"
-          d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"
-        ></path>
-        <path
-          fill="#34A853"
-          d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"
-        ></path>
-        <path fill="none" d="M0 0h48v48H0z"></path>
-      </g>
-    </svg>
-  );
-};
-
 export type LoginScreenState = "HIDDEN" | "LOGIN" | "SIGNUP";
 
 export const useLoginScreen = () => {
@@ -115,14 +72,7 @@ export const LoginScreen = ({
           <CloseSvg />
           <span className="sr-only">Close</span>
         </button>
-        <button
-          className="hidden rounded-2xl border-2 border-b-4 border-gray-200 px-4 py-3 text-sm font-bold uppercase text-blue-400 transition hover:bg-gray-50 hover:brightness-90 sm:block"
-          onClick={() =>
-            setLoginScreenState((x) => (x === "LOGIN" ? "SIGNUP" : "LOGIN"))
-          }
-        >
-          {loginScreenState === "LOGIN" ? "Sign up" : "Login"}
-        </button>
+        
       </header>
       <div className="flex grow items-center justify-center">
         <div className="flex w-full flex-col gap-5 sm:w-96">
@@ -174,26 +124,17 @@ export const LoginScreen = ({
               className="grow rounded-2xl border-2 border-gray-200 bg-gray-50 px-4 py-3"
               placeholder={
                 loginScreenState === "LOGIN"
-                  ? "Email or username (optional)"
-                  : "Email (optional)"
+                  ? "DNI"
+                  : "DNI"
               }
             />
             <div className="relative flex grow">
               <input
                 className="grow rounded-2xl border-2 border-gray-200 bg-gray-50 px-4 py-3"
-                placeholder="Password (optional)"
+                placeholder="Contraseña"
                 type="password"
               />
-              {loginScreenState === "LOGIN" && (
-                <div className="absolute bottom-0 right-0 top-0 flex items-center justify-center pr-5">
-                  <Link
-                    className="font-bold uppercase text-gray-400 hover:brightness-75"
-                    href="/forgot-password"
-                  >
-                    Forgot?
-                  </Link>
-                </div>
-              )}
+              
             </div>
           </div>
           <button
@@ -202,59 +143,6 @@ export const LoginScreen = ({
           >
             {loginScreenState === "LOGIN" ? "Log in" : "Create account"}
           </button>
-          <div className="flex items-center gap-2">
-            <div className="h-[2px] grow bg-gray-300"></div>
-            <span className="font-bold uppercase text-gray-400">or</span>
-            <div className="h-[2px] grow bg-gray-300"></div>
-          </div>
-          <div className="flex gap-5">
-            <button
-              className="flex w-full items-center justify-center gap-2 rounded-2xl border-2 border-b-4 border-gray-200 py-3 font-bold text-blue-900 transition hover:bg-gray-50 hover:brightness-90"
-              onClick={logInAndSetUserProperties}
-            >
-              <FacebookLogoSvg className="h-5 w-5" /> Facebook
-            </button>
-            <button
-              className="flex w-full items-center justify-center gap-2 rounded-2xl border-2 border-b-4 border-gray-200 py-3 font-bold text-blue-600 transition hover:bg-gray-50 hover:brightness-90"
-              onClick={logInAndSetUserProperties}
-            >
-              <GoogleLogoSvg className="h-5 w-5" /> Google
-            </button>
-          </div>
-          <p className="text-center text-xs leading-5 text-gray-400">
-            By signing in to Duolingo, you agree to our{" "}
-            <Link
-              className="font-bold"
-              href="https://www.duolingo.com/terms?wantsPlainInfo=1"
-            >
-              Terms
-            </Link>{" "}
-            and{" "}
-            <Link
-              className="font-bold"
-              href="https://www.duolingo.com/privacy?wantsPlainInfo=1"
-            >
-              Privacy Policy
-            </Link>
-            .
-          </p>
-          <p className="text-center text-xs leading-5 text-gray-400">
-            This site is protected by reCAPTCHA Enterprise and the Google{" "}
-            <Link
-              className="font-bold"
-              href="https://policies.google.com/privacy"
-            >
-              Privacy Policy
-            </Link>{" "}
-            and{" "}
-            <Link
-              className="font-bold"
-              href="https://policies.google.com/terms"
-            >
-              Terms of Service
-            </Link>{" "}
-            apply.
-          </p>
           <p className="block text-center sm:hidden">
             <span className="text-sm font-bold text-gray-700">
               {loginScreenState === "LOGIN"
