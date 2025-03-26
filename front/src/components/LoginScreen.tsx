1fbbb6f17b5dc7d722ffb85387081bd44f3b941d
Fedesin <fedesimone31@gmail.com>
2025-03-26 11:39:13 -0300

    personalizo un poco la visual de la pantalla iniciar sesion

--- front/src/components/LoginScreen.tsx
+++ front/src/components/LoginScreen.tsx
@@ -5,7 +5,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { useBoundStore } from "~/hooks/useBoundStore";
 import { useRouter } from "next/router";
 
-export type LoginScreenState = "HIDDEN" | "LOGIN" | "SIGNUP";
+export type LoginScreenState = "HIDDEN" | "LOGIN";
 
 export const useLoginScreen = () => {
   const router = useRouter();
@@ -13,7 +13,6 @@ export const useLoginScreen = () => {
   const queryState: LoginScreenState = (() => {
     if (loggedIn) return "HIDDEN";
     if ("login" in router.query) return "LOGIN";
-    if ("sign-up" in router.query) return "SIGNUP";
     return "HIDDEN";
   })();
   const [loginScreenState, setLoginScreenState] = useState(queryState);
@@ -113,49 +112,9 @@ export const LoginScreen = ({
       <div className="flex grow items-center justify-center">
         <div className="flex w-full flex-col gap-5 sm:w-96">
           <h2 className="text-center text-2xl font-bold text-gray-800">
-            {loginScreenState === "LOGIN" ? "Log in" : "Create your profile"}
+            {loginScreenState === "LOGIN" ? "Iniciar sesion" : "Iniciar sesion"}
           </h2>
           <div className="flex flex-col gap-2 text-black">
-            {loginScreenState === "SIGNUP" && (
-              <>
-                <div className="relative flex grow">
-                  <input
-                    className="grow rounded-2xl border-2 border-gray-200 bg-gray-50 px-4 py-3"
-                    placeholder="Age (optional)"
-                  />
-                  <div className="absolute bottom-0 right-0 top-0 flex items-center justify-center pr-4">
-                    <div
-                      className="relative flex h-6 w-6 cursor-pointer items-center justify-center rounded-full border-2 border-gray-200 text-gray-400"
-                      onMouseEnter={() => setAgeTooltipShown(true)}
-                      onMouseLeave={() => setAgeTooltipShown(false)}
-                      onClick={() => setAgeTooltipShown((x) => !x)}
-                      role="button"
-                      tabIndex={0}
-                      aria-label="Why do you need an age?"
-                    >
-                      ?
-                      {ageTooltipShown && (
-                        <div className="absolute -right-5 top-full z-10 w-72 rounded-2xl border-2 border-gray-200 bg-white p-4 text-center text-xs leading-5 text-gray-800">
-                          Providing your age ensures you get the right Duolingo
-                          experience. For more details, please visit our{" "}
-                          <Link
-                            href="https://www.duolingo.com/privacy"
-                            className="text-blue-700"
-                          >
-                            Privacy Policy
-                          </Link>
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                </div>
-                <input
-                  className="grow rounded-2xl border-2 border-gray-200 bg-gray-50 px-4 py-3"
-                  placeholder="Name (optional)"
-                  ref={nameInputRef}
-                />
-              </>
-            )}
             <input
               className="grow rounded-2xl border-2 border-gray-200 bg-gray-50 px-4 py-3"
               placeholder="DNI"
@@ -176,24 +135,9 @@ export const LoginScreen = ({
             className="rounded-2xl border-b-4 border-blue-500 bg-blue-400 py-3 font-bold uppercase text-white transition hover:brightness-110"
             onClick={logInAndSetUserProperties}
           >
-            {loginScreenState === "LOGIN" ? "Log in" : "Create account"}
+            {loginScreenState === "LOGIN" ? "Aceptar" : "Aceptar"}
           </button>
           <p className="block text-center sm:hidden">
-            <span className="text-sm font-bold text-gray-700">
-              {loginScreenState === "LOGIN"
-                ? "Don't have an account?"
-                : "Have an account?"}
-            </span>{" "}
-            <button
-              className="text-sm font-bold uppercase text-blue-400"
-              onClick={() =>
-                setLoginScreenState((x) =>
-                  x === "LOGIN" ? "SIGNUP" : "LOGIN"
-                )
-              }
-            >
-              {loginScreenState === "LOGIN" ? "sign up" : "log in"}
-            </button>
           </p>
         </div>
       </div>
