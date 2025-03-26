269b4967d8583159b7ab3d7ec7fffc04bea974dd
Fedesin <fedesimone31@gmail.com>
2025-03-26 11:26:50 -0300

    edito el index así dice unlu y eso

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -22,20 +22,15 @@ const Home: NextPage = () => {
         <GlobeSvg className="h-fit w-7/12 md:w-[360px]" />
         <div>
           <p className="mb-6 max-w-[600px] text-center text-3xl font-bold md:mb-12">
-            The free, fun, and effective way to learn a language!
+            Taller de resolución de problemas, Licenciatura en sistemas de información
           </p>
           <div className="mx-auto mt-4 flex w-fit flex-col items-center gap-3">
-            <Link
-              href="/register"
-              className="w-full rounded-2xl border-b-4 border-green-700 bg-green-600 px-10 py-3 text-center font-bold uppercase transition hover:border-green-600 hover:bg-green-500 md:min-w-[320px]"
-            >
-              Get started
-            </Link>
+            
             <button
               className="w-full rounded-2xl border-2 border-b-4 border-[#042c60] bg-[#235390] px-8 py-3 font-bold uppercase transition hover:bg-[#204b82] md:min-w-[320px]"
               onClick={() => setLoginScreenState("LOGIN")}
             >
-              I already have an account
+              Iniciar sesion
             </button>
           </div>
         </div>
